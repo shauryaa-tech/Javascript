c8d76e546f2969f96ab354eeadcca7e13d46e523
Shaurya Pratap Singh <shaurya13822@gmail.com>
2025-06-11 14:57:26 +0000

    datatypes

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144365
+let accountEmail = "shaurya@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 5// not allowed 
+
+accountEmail = "shaurya22@google.com"
+accountPassword = "22222222"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // trat all JS code as newer version
+
+console.log(3 + 3)
+
+console.log("shauryaa")
+
+let name = "shaurya"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power53
+// bigint
+// string => ""
+// bollean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
