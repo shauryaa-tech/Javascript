7108cf80462b635fde71af47ca04e91b6b4a55bf
Shaurya Pratap Singh <shaurya13822@gmail.com>
2025-06-12 14:24:57 +0000

    co

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -20,3 +20,46 @@ console.log(booleanIsLoggedIn);
 // true => 1; false => 0
 // "" => false
 // "shauryaa" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ************************ Operation *****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " shauryaa"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2  + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCOunter = 100
+gameCOunter++;
+console.log(gameCOunter);
