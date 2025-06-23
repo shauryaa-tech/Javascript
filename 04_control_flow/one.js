0d3a4512c94a85341e67a47fc68419d0c581bf7e
Shaurya Pratap Singh <shaurya13822@gmail.com>
2025-06-23 16:39:15 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+// // if
+// const isUserLoggedIn = true
+// const tempreature = 41
+
+// if ( tempreature === 40 ){
+//     console.log("less than 50");   
+// } else{
+//     console.log("tempreature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+if (score > 100) {
+    const 
+}
