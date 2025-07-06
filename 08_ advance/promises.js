a53253e682bd3938555e80b160b9e70a9c22485c
Shaurya Pratap Singh <shaurya13822@gmail.com>
2025-07-06 15:41:28 +0000

    promises

--- 08_ advance/promises.js
+++ 08_ advance/promises.js
@@ -95,6 +95,11 @@ consumePromiseFive()
 
 // getAllUsers()
 
+
+// The Fetch API provides a JavaScript interface for making HTTP requests and processing the responses.
+/* Fetch is the modern replacement for XMLHttpRequest: unlike XMLHttpRequest, which uses callbacks, Fetch is promise-based and is 
+integrated with features of the modern web such as service workers and Cross-Origin Resource Sharing (CORS).*/
+
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => {
     return response.json()
