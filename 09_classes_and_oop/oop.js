32cd0b113bf804a5e636d57b87d9cc8db867bad7
Shaurya Pratap Singh <shaurya13822@gmail.com>
2025-07-07 11:41:19 +0000

    oop

--- 09_classes_and_oop/oop.js
+++ 09_classes_and_oop/oop.js
@@ -11,5 +11,28 @@ const user = {
     
 }
 
-console.log(user.username);
-console.log(user.getUserDetails);
+
+
+// console.log(user.username);
+// console.log(user.getUserDetails);
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    } 
+
+    return this 
+}
+
+const userOne = new User("Shaurya", 12, true)  // after writing new it create new object
+const userTwo = new User("Shaurya-tech", 11, false)
+console.log(userOne.constructor);
+// console.log(userOne);
+// console.log(userTwo);
